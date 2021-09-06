d5907505d10db6b8c78a879760e4a36d8d6db185
joannasimms <61862539+joannasimms@users.noreply.github.com>
2021-09-06 22:25:34 +0300

    tax.R documentation updated

--- agentmodel/R/after.tax.R
+++ agentmodel/R/after.tax.R
@@ -1,14 +1,11 @@
 #' After Tax
 #' 
-#' works out the money let after tax.
-#' TODO: check that the tax rate is right and what it should be applied to
+#' works out the money let after tax. According to OED tax is 20%.
 #' 
 #' @export
 
 
 after.tax <- function(a) {
-  # income tax in Finland is 20%, I think this is for businesses
-  # https://www.oecd-ilibrary.org/sites/e9c6c9d1-en/index.html?itemId=/content/component/e9c6c9d1-en
   tax = 0.8*(a)
   return(tax)
 }
